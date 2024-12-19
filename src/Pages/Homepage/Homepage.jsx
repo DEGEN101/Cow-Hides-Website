0978d135c159464d0160e54f0a10d3a91c8fd708
DEGEN101 <ntando.raji13@gmail.com>
2024-12-19 23:14:20 +0200

    Created Package Card Component

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,12 +1,32 @@
+import PackageCard from "../../Components/PackageCard/PackageCard";
 import Footer from "../../Components/Footer/Footer";
 
 import "./Homepage.css"
 
 const Homepage = () => {
+
+    const premiumPackageInfo = {
+        TITLE: "Premium  Package",
+        SUBTITLE: "Export & Import",
+        CONTENT: [
+            "3000 cow hides",
+            "40-ft container", 
+            "Average Weight: 8.5 kg per hide for superior resilience",
+            "Dedicated Customer Support via Phone for Quick Resolutions",
+            "24/7 Email Assistance for Your Convenience"
+        ]
+    };
+
+    // HTML Code
     return (
         <>
             <h1>Homepage</h1>
+            <section className="homepage-centre-container">
 
+                <section className="packages-section">
+                    <PackageCard packageInfo={premiumPackageInfo}/>
+                </section>
+            </section>
             <Footer/>
         </>
     );
